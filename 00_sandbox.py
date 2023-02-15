c43bdd1c4ef2a399c3fca2d40b830eff09d72b8e
smithr1057 <108771978+smithr1057@users.noreply.github.com>
2023-02-16 10:50:44 +1300

    Add files via upload

---
+++ 00_sandbox.py
@@ -0,0 +1,8 @@
+# Ask users for a number
+get_num = int(input("Enter an integer: "))
+
+# Multiply the number by 5
+times_five = get_num * 5
+
+# Output the result
+print("{} times five equals {}".format(get_num, times_five))
