3f509fa4dd1770e38c2d7a7fe2c884356735aee7
smithr1057 <108771978+smithr1057@users.noreply.github.com>
2023-02-17 09:57:17 +1300

    Add files via upload

--- 01_yes_no_v1.py
+++ 01_yes_no_v1.py
@@ -1,5 +1,22 @@
-# Ask the user if they have played before
 
-# If they say yes, output "program continues"
+show_instructions = ""
+while show_instructions.lower() != "xxx":
 
-# If they say no , output 'display instructions'
+    # Ask the user if they have played before
+    show_instructions = input("Have you played this game before? ").lower()
+
+    # If they say yes, output "program continues"
+    program_cont = ["yes", "y"]
+
+    if show_instructions in program_cont:
+        print("Program continues")
+
+    # If they say no , output 'display instructions'
+    instructions = ["no", "n"]
+
+    if show_instructions in instructions:
+        print("***INSTRUCTIONS***")
+
+    # If they say anything else then 'error please enter yes / no'
+    else:
+        print(" **ERROR** please enter either yes / no")
