afac2fd8542006e6d757ab84fe611c42744d8c20
smithr1057 <108771978+smithr1057@users.noreply.github.com>
2023-02-22 12:30:30 +1300

    Add files via upload

---
+++ 04_how_much_v1.py
@@ -0,0 +1,13 @@
+# Functions go here
+
+
+# Main routine goes here
+
+error = "Please enter a whole number between 1 and 10"
+
+While True:
+    # Ask the question
+    response = int(input("How much would you like to play with? "))
+
+    # if the amount is too low / too high give error
+
