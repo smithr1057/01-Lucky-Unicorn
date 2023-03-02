4898d071f43df6fe54ce98510e528c55c99fc4c6
smithr1057 <108771978+smithr1057@users.noreply.github.com>
2023-03-03 10:06:19 +1300

    Add files via upload

--- 00_LU_Base_v_01.py
+++ 00_LU_Base_v_01.py
@@ -25,7 +25,19 @@ def instructions():
     statement_generator(" How to Play ", "*")
     print()
     print("Choose a starting amount, minimum $1, maximum $10.")
-    print("Then press <enter> to play.")
+    print()
+    print("Then press <enter> to play. You will get either a horse, a zebra, a donkey, or a unicorn.")
+    print()
+    print("It costs $1 per round. Depending on your prize you might win some of your money back. Here's the payout "
+          "amounts...")
+    print("Unicorn: +$4.00")
+    print("Horse: -$0.50")
+    print("Zebra: -$0.50")
+    print("Donkey: -$1")
+    print()
+    print("Can you avoid the donkeys, and win it big with the unicorns?")
+    print()
+    print("Hint: to quit while you are ahead, type 'xxx' instead f pressing <enter>")
     print()
     return ""
 
@@ -68,14 +80,21 @@ def statement_generator(statement, decoration):
 
 
 # Main Routine goes here...
+statement_generator("Welcome to the Lucky Unicorn game", "*")
+print()
+
 played_before = yes_no("Have you played the game before? ")
+print()
 
 
 if played_before == "no":
     instructions()
 
+statement_generator("Let's play a game", "-")
+print()
 # Ask user how much they want to play with...
 how_much = num_check("How much would you like to play with? ", 0, 10)
+print(f"You are playing with ${how_much}")
 
 balance = how_much
 
@@ -98,12 +117,14 @@ while play_again == "":
     # user gets a unicorn (add $4 to balance)
     if 1 <= chosen_num <= 5:
         chosen = "unicorn"
+        prize_decoration = "!"
         balance += 4
 
     # if the random # is between 6 and 36
     # user gets a donkey (subtract $1 from balance
     elif 6 <= chosen_num <= 36:
         chosen = "donkey"
+        prize_decoration = "D"
         balance -= 1
 
     # The token is either a horse or a zebra...
@@ -113,23 +134,29 @@ while play_again == "":
         # item to a horse
         if chosen_num % 2 == 0:
             chosen = "horse"
+            prize_decoration = "H"
 
         # otherwise set it to a zebra
         else:
             chosen = "zebra"
+            prize_decoration = "Z"
         balance -= .5
 
-    print(f"You got a {chosen}. Your balance is ${balance:.2f}")
+    outcome = f"You got a {chosen}. Your balance is ${balance:.2f}"
+    statement_generator(outcome, prize_decoration)
 
     if balance < 1:
         # If balance is too low, exit the game and
         # output a suitable message
         play_again = "xxx"
         print()
-        print("Sorry you have run out of money")
+        statement_generator("Sorry you have run out of money", "x")
     else:
         play_again = input("Press Enter to play again or 'xxx' to quit ")
 
+
 print()
+statement_generator("Results", "*")
+print(f"Final balance ${balance}")
 print("Thank you for playing the Lucky Unicorn game")
-print(f"*** Your final balance is ${balance} ***")
+
